1aa5efdfcde2612f7d904bb78c86aa68030fd276
elfixit <el@xiala.net>
2014-05-26 18:25:11 +0200

    initial commit

--- output_wallofsheep.lua
+++ output_wallofsheep.lua
@@ -22,27 +22,52 @@ output_wallofsheep = pom.output.new("wallofsheep", "Log clear text password seen
 })
 
 
-function output_wallofsheep:process_http_request(evt)
+function output_wallofsheep:process_http_request_auth(evt)
 
-	local data = evt.data
+    local data = evt.data
 
-	local username = data["username"]
-	local password = data["password"]
+    local username = data["username"]
+    local password = data["password"]
 
-	if not username or not password then return end
+    if not username or not password then return end
 
-	local client = data["client_addr"]
-	local server = data["server_name"]
-	local status = data["status"]
+    local client = data["client_addr"]
+    local server = data["server_name"]
+    local status = data["status"]
 
-	if not status
-	then
-		status = "unknown"
-	end	
+    if not status
+    then
+        status = "unknown"
+    end
 
 
-	self.logfile:write("Found credentials via HTTP : " .. client .. " -> " .. server .. " | user : '" .. username .."', password : '" .. password .. "' (status " .. status .. ")\n")
-	self.logfile:flush()
+    self.logfile:write("Found credentials via HTTP : " .. client .. " -> " .. server .. " | user : '" .. username .."', password : '" .. password .. "' (status " .. status .. ")\n")
+    self.logfile:flush()
+
+end
+
+function output_wallofsheep:process_http_request_post(evt)
+
+    local data = evt.data
+
+    if not data['request_method'] == 'POST' then return end
+
+    if not username or not password then return end
+
+    local username = data['post_data']
+
+    local client = data["client_addr"]
+    local server = data["server_name"]
+    local status = data["status"]
+
+    if not status
+    then
+        status = "unknown"
+    end
+
+
+    self.logfile:write("Found credentials via HTTP : " .. client .. " -> " .. server .. " | user : '" .. username .."', password : '" .. password .. "' (status " .. status .. ")\n")
+    self.logfile:flush()
 
 end
 
@@ -125,7 +150,8 @@ function output_wallofsheep:open()
 	self.logfile = io.open(self:param_get("log_file"), "a")
 
 	-- Listen to HTTP request event
-	self:event_listen_start("http_request", nil, self.process_http_request)
+	self:event_listen_start("http_request", nil, self.process_http_request_auth)
+    self:event_listen_start("http_request", nil, self.process_http_request_post)
 
 	-- Listen to SMTP auth event
 	self:event_listen_start("smtp_auth", nil, self.process_smtp_auth)
