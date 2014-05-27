495f288e43e9bdecf8a6e84df64d594b0b95ba39
elfixit <el@xiala.net>
2014-05-27 14:36:41 +0200

    remove debug code

--- output_wallofsheep.lua
+++ output_wallofsheep.lua
@@ -56,7 +56,7 @@ function output_wallofsheep:process_http_request_post(evt)
 
     local data = evt.data
     if data['request_method'] == 'GET' then return end
-    
+
     local password, post_str
     post_str = "{"
     local data_iter = pom.data.item_iterator(data['post_data'])
@@ -64,7 +64,7 @@ function output_wallofsheep:process_http_request_post(evt)
         local key, value
         key, value = data_iter()
         if not key then break end
-        print("key: " .. key .. " value: " .. value)
+        --print("key: " .. key .. " value: " .. value)
         if key == "password" or key == "passwd" or key == "pass" then
             password = value
         end
